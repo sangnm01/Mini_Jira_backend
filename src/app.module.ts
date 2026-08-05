a07ddfc425cdcd23d088820ff4f89cceee3fb246
sang.nguyenmanh <sang.nguyenmanh@vti.com.vn>
2026-08-05 22:20:07 +0700

    Connect database + design modal users

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './modules/users/users.module';
 import { ProjectsModule } from './modules/projects/projects.module';
 
 @Module({
   imports: [UsersModule, ProjectsModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
