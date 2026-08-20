9da43775f3238f24cf45a0a97c854834bd65f31e
sang.nguyenmanh <sang.nguyenmanh@vti.com.vn>
2026-08-20 22:18:08 +0700

    get all user

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -6,23 +6,24 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
+  Query,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
-  }
-
+  @UseGuards(JwtAuthGuard)
   @Get()
-  findAll() {
-    return this.usersService.findAll();
+  findAll(
+    @Query('page') page: number = 1,
+    @Query('limit') limit: number = 10,
+  ) {
+    return this.usersService.findAll(Number(page), Number(limit));
   }
 
   @Get(':id')
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,18 +1,46 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(private readonly prisma: PrismaService) {}
+
+  async findAll(page: number, limit: number) {
+    const skip = (page - 1) * limit;
+
+    const listUser = await this.prisma.user.findMany({
+      skip,
+      take: limit,
+      select: {
+        id: true,
+        username: true,
+        email: true,
+        fullName: true,
+        avatar: true,
+        role: true,
+        isActive: true,
+        createdAt: true,
+      }
+    });
+
+    const totalUsers = await this.prisma.user.count();
+
+    const totalPages = Math.ceil(totalUsers / limit);
+
+    return {
+      users: listUser,
+      pagination: {
+        page,
+        limit,
+        total: totalUsers,
+        totalPages,
+      },
+    };
   }
 
-  findAll() {
-    return `This action returns all users`;
-  }
-
-  findOne(id: number) {
+  async findOne(id: number) {
     return `This action returns a #${id} user`;
   }
 
